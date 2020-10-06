feec25a8d08a5061a1c150ccb5dae506ce7a1710
Alex Stewart <astewart1510@gmail.com>
2020-10-06 12:19:32 +0200

    New financials web app

---
+++ financials/README.md
@@ -0,0 +1,34 @@
+# financials
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+
+
+
+SETUP NEW PROJECT:
+
+npm install bootstrap-vue
+
+
+npm install vue-material --save
+
+
+npm install axios
---
+++ financials/src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+//import BootstrapVue
+import  BootstrapVue  from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+//import Vue Matertial components
+import { MdButton, MdContent, MdTabs, MdList, MdIcon, MdBadge } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdList)
+Vue.use(MdIcon)
+Vue.use(MdBadge)
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
